0b98055e2ee1a72976994e00e866c9b0a050d7e8
nadineb <nadineb@umich.edu>
2020-04-19 15:51:19 -0700

    [add] 's' to table names and insert values

--- schema.sql
+++ schema.sql
@@ -3,22 +3,28 @@ CREATE database employeeDB;
 
 USE employeeDB;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(30) NOT NULL
-)
+);
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    department_id INT(10) NOT NULL
-)
+    department_id INT(10) NULL
+);
 
-CREATE TABLE employee (
+INSERT INTO roles (title, salary)
+VALUES ("engineer", 100000), ("sales", 80000);
+
+CREATE TABLE employees (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT(10) NOT NULL,
     manager_id INT(10) NULL
-)
+);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("nadine", "bundschuh", 1, 2), ("john", "bundschuh", 2);
