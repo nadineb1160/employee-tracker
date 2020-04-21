fd1e6d13e4e389b1443802ddd8b25955547cc57d
nadineb <nadineb@umich.edu>
2020-04-20 21:52:23 -0700

    [add] specific id names

--- schema.sql
+++ schema.sql
@@ -4,27 +4,21 @@ CREATE database employeeDB;
 USE employeeDB;
 
 CREATE TABLE departments (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    department_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    role_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT(10) NULL
 );
 
-INSERT INTO roles (title, salary)
-VALUES ("engineer", 100000), ("sales", 80000);
-
 CREATE TABLE employees (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    employee_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT(10) NOT NULL,
+    role_id INT(10) NOT NULL, 
     manager_id INT(10) NULL
 );
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("nadine", "bundschuh", 1, 2), ("john", "bundschuh", 2);
