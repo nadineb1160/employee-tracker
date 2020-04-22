18e3d17bb0dae0c6d663ecc78f85061641c06669
nadineb <nadineb@umich.edu>
2020-04-21 19:19:58 -0700

    [add] foreign keys

--- schema.sql
+++ schema.sql
@@ -6,13 +6,15 @@ USE employeeDB;
 CREATE TABLE departments (
     department_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(30) NOT NULL
+    PRIMARY KEY (department_id)
 );
 
 CREATE TABLE roles (
     role_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    department_id INT(10) NULL
+    department_id INT(10) NULL,
+    FOREIGN KEY (department_id) REFERENCES departments (department_id),
 );
 
 CREATE TABLE employees (
@@ -20,5 +22,7 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT(10) NOT NULL, 
-    manager_id INT(10) NULL
+    manager_id INT(10) NULL,
+    FOREIGN KEY (role_id) REFERENCES role (role_id),
+    FOREIGN KEY (manager_id) REFERENCES employees (employee_id),
 );
